8865594ec17312b590ae24c3bc318a31fe367e3c
JaPod993 <j.pod.12@gmail.com>
2017-04-12 14:00:32 +0200

    wspolpraca final

--- ekopsychologia/apps/website/static/website/js/main.js
+++ ekopsychologia/apps/website/static/website/js/main.js
@@ -1,13 +1,13 @@
-$(function(){
-    $("#newsletter-sign-in").on("submit", function(e){
+$(function () {
+    $("#newsletter-sign-in").on("submit", function (e) {
         e.preventDefault();
         var form = $(this);
         form.find(".msg").remove();
         $.post(form.attr("action"), form.serialize(), function (data) {
             console.log(data);
             if (data.status == "OK") {
-                    form.find("input[name='email']").val("").after('<span class="msg success">' + data.success + '</span>');
-            }else if (data.status == "NOK") {
+                form.find("input[name='email']").val("").after('<span class="msg success">' + data.success + '</span>');
+            } else if (data.status == "NOK") {
                 form.find("input[name='email']").after('<span class="msg error">' + data.error + '</span>');
             }
         }).fail(function (data) {
@@ -18,70 +18,68 @@ $(function(){
 })
 
 
+$(document).ready(function () {
 
+    $('.single-item').slick({
 
- $(document).ready(function(){
-
-        $('.single-item').slick({
-
-            accessibility: true,
-            dots: true,
-            autoplay: false,
-            autoplaySpeed: 5000,
-        });
-        $('.partner-1').slick({
-           dots: false,
-            arrows: false,
-            autoplay: true,
-            fade: true,
-            centerMode: true,
-            autoplaySpeed: 5000,
-        });
-
+        accessibility: true,
+        dots: true,
+        autoplay: false,
+        autoplaySpeed: 5000,
+    });
+    $('.partner-1').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        fade: true,
+        centerMode: true,
+        autoplaySpeed: 5000,
     });
 
-    //article height
+});
 
-    function set_news_height() {
-        var sizes = [];
-        $(".compared-text-part2").height('auto');
-        $(".compared-text-part").height('auto');
+//article height
 
-        $(".compared-text-part").each(function(){
-            sizes.push($(this).height());
-        });
-        $(".compared-text-part").height(Math.max.apply(null, sizes));
-        $(".compared-text-part2").each(function(){
-            sizes.push($(this).height());
-        });
-        $(".compared-text-part2").height(Math.max.apply(null, sizes));
-    }
+function set_news_height() {
+    var sizes = [];
+    $(".compared-text-part2").height('auto');
+    $(".compared-text-part").height('auto');
 
-    function is_desktop(){
-        return $(window).width() >= 736;
-    }
+    $(".compared-text-part").each(function () {
+        sizes.push($(this).height());
+    });
+    $(".compared-text-part").height(Math.max.apply(null, sizes));
+    $(".compared-text-part2").each(function () {
+        sizes.push($(this).height());
+    });
+    $(".compared-text-part2").height(Math.max.apply(null, sizes));
+}
 
-    $(function(){
-        var resize_timeout = null;
+function is_desktop() {
+    return $(window).width() >= 736;
+}
 
-        setTimeout(function(){
-            if(is_desktop()){
+$(function () {
+    var resize_timeout = null;
+
+    setTimeout(function () {
+        if (is_desktop()) {
+            set_news_height();
+        }
+    }, 200);
+
+    $(window).resize(function () {
+        if (is_desktop()) {
+
+            if (resize_timeout != null) {
+                clearTimeout(resize_timeout);
+            }
+
+            resize_timeout = setTimeout(function () {
                 set_news_height();
-            }
-        }, 200);
+            }, 200);
 
-        $(window).resize(function() {
-            if(is_desktop()){
-
-                if(resize_timeout != null){
-                    clearTimeout(resize_timeout);
-                }
-
-                resize_timeout = setTimeout(function(){
-                    set_news_height();
-                }, 200);
-
-            }
-        });
-
+        }
     });
+
+});
