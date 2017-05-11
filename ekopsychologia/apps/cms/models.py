2433a3ca21a4e4a65da074b0bdfa898c69239b5c
JaPod993 <j.pod.12@gmail.com>
2017-05-11 11:06:41 +0200

    ads

--- ekopsychologia/apps/cms/models.py
+++ ekopsychologia/apps/cms/models.py
@@ -23,6 +23,7 @@ class Site(SiteAbstract):
             ('website/site/templates/site_obszary_dzialalnosci.html', u'Obszary działania'),
             ('website/site/templates/site_contact.html', u'Kontakt'),
             ('website/site/templates/site_partnership.html', u'Wspolpraca'),
+
     ]
 
     def allow_redirect_to_children(self):
