01355d752febcd83c5ad6725b1d98fa75340738e
JaPod993 <j.pod.12@gmail.com>
2017-05-16 10:24:13 +0200

    too;bar

--- ekopsychologia/urls.py
+++ ekopsychologia/urls.py
@@ -24,3 +24,8 @@ urlpatterns = [
     url(r'^', include('corecms.urls.site', namespace='corecms')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns = [
+#         url(r'^__debug__/', include(debug_toolbar.urls)),
+#     ] + urlpatterns
