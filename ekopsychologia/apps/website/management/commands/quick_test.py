ba0822b50badb645c5481eb55d7fbf51aba92b88
JaPod993 <j.pod.12@gmail.com>
2017-05-17 15:06:20 +0200

    x

--- ekopsychologia/apps/website/management/commands/quick_test.py
+++ ekopsychologia/apps/website/management/commands/quick_test.py
@@ -2,6 +2,7 @@
 import sys
 from shutil import copyfile
 
+from corecms.forms.forms import get_slug_for_site_or_article
 from corecms.models.gallery import Gallery
 from django.core.files.uploadedfile import UploadedFile, SimpleUploadedFile
 from django.core.management.base import BaseCommand
@@ -9,6 +10,10 @@ import os, errno
 from django.core.files import File
 from io import BytesIO
 from PIL import Image
+from django.template.defaultfilters import slugify
+from unidecode import unidecode
+
+from cms.models import Article, Site
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -19,4 +24,14 @@ class Command(BaseCommand):
     help = ''
 
     def handle(self, *args, **options):
-        pass
+        for a in Article.objects.filter(identity__contains='"'):
+            slug = orig = slugify(unidecode(a.identity)[:200])
+            a.slug = get_slug_for_site_or_article(a, slug, orig, 200)
+            a.save()
+            print a.slug
+
+        for a in Site.objects.filter(identity__contains='"'):
+            slug = orig = slugify(unidecode(a.identity)[:200])
+            a.slug = get_slug_for_site_or_article(a, slug, orig, 200)
+            a.save()
+            print a.slug
